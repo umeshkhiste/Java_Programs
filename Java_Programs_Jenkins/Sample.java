11ca64aac09a9da67d5f5fbfd8da4de56b4aa682
umeshkhiste <umesh.khiste@gmail.com>
2023-12-26 17:32:17 -0800

    Update Sample.java

--- Java_Programs_Jenkins/Sample.java
+++ Java_Programs_Jenkins/Sample.java
@@ -3,7 +3,7 @@ import java.util.Date;
 public class Sample {
     public static void main(String[] args) {
         System.out.println("This is java program");
-        System.out.println("Adding  line");
+        System.out.println("Adding one line");
         System.out.println(new Date());
     }
 }
