56ec4d2dbc31df1fc3483b0b1c0dc5edb8564c22
umeshkhiste <umesh.khiste@gmail.com>
2023-12-26 17:19:01 -0800

    Create Sample.java
    Added new program to check jenkins

---
+++ Java_Programs_Jenkins/Sample.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class Sample {
+    public static void main(String[] args) {
+        System.out.println("This is java program");
+        System.out.println(new Date());
+    }
+}
