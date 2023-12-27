d8d9e193ebbc2faefb1779119d6d5086c9773d39
umeshkhiste <umesh.khiste@gmail.com>
2023-12-26 17:21:51 -0800

    line added

--- Java_Programs_Jenkins/Sample.java
+++ Java_Programs_Jenkins/Sample.java
@@ -3,6 +3,7 @@ import java.util.Date;
 public class Sample {
     public static void main(String[] args) {
         System.out.println("This is java program");
+        System.out.println("Adding new line");
         System.out.println(new Date());
     }
 }
